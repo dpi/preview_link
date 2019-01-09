d57f245b8551f16e0835d78d1648ebaa85d07297
larowlan <larowlan@395439.no-reply.drupal.org>
2019-01-09 15:39:52 +1000

    Issue #2903728 by kim.pepper, larowlan: Cron logic is back to front

--- tests/src/Kernel/PreviewLinkExpiryTest.php
+++ tests/src/Kernel/PreviewLinkExpiryTest.php
@@ -30,11 +30,15 @@ class PreviewLinkExpiryTest extends PreviewLinkBase {
    * Test preview links are automatically expired on cron.
    */
   public function testPreviewLinkExpires() {
+    $days = \Drupal::state()->get('preview_link_expiry_days', 7);
+    // Add an extra day to make it expired.
+    $days = $days + 1;
+    $days_in_seconds = $days * 86400;
     $expired_preview_link = PreviewLink::create([
       'entity_type_id' => 'node',
       'entity_id' => $this->node->id(),
       // Set a timestamp that will definitely be expired.
-      'generated_timestamp',
+      'generated_timestamp' => $days_in_seconds,
     ]);
     $expired_preview_link->save();
     $id = $expired_preview_link->id();
@@ -45,4 +49,3 @@ class PreviewLinkExpiryTest extends PreviewLinkBase {
   }
 
 }
-
