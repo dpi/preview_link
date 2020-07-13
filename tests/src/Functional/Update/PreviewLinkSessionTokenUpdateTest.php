ff16805a1faf984f7ea7402c16eee3e1b289ea0f
dpi <pro@danielph.in>
2020-07-13 11:00:56 +0800

    https://www.drupal.org/files/issues/2020-07-12/3155009-32.patch

--- tests/src/Functional/Update/PreviewLinkSessionTokenUpdateTest.php
+++ tests/src/Functional/Update/PreviewLinkSessionTokenUpdateTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Drupal\Tests\preview_link\Functional;
+namespace Drupal\Tests\preview_link\Functional\Update;
 
 use Drupal\entity_test\Entity\EntityTestMulRevPub;
 use Drupal\FunctionalTests\Update\UpdatePathTestBase;
@@ -38,7 +38,7 @@ class PreviewLinkSessionTokenUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles(): void {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../../../core/modules/system/tests/fixtures/update/drupal-8.8.0.bare.standard.php.gz',
+      DRUPAL_ROOT . '/core/modules/system/tests/fixtures/update/drupal-8.8.0.bare.standard.php.gz',
       __DIR__ . '/../../../../tests/fixtures/update/preview-link-multi-3155009.php',
     ];
   }
@@ -49,16 +49,17 @@ class PreviewLinkSessionTokenUpdateTest extends UpdatePathTestBase {
    * @see \preview_link_update_8201()
    * @see \preview_link_update_8202()
    * @see \preview_link_update_8203()
-   * @see \preview_link_post_update_0001_entities_field()
-   * @see \preview_link_post_update_0002_migrate_entity_references()
-   * @see \preview_link_post_update_0003_delete_fields()
+   * @see \preview_link_update_8204()
+   * @see \preview_link_update_8205()
+   * @see \preview_link_update_8206()
    */
   public function testMultipleEntitiesFieldMigration(): void {
     EntityTestMulRevPub::create([
       'id' => 2,
     ])->save();
 
-    $dbSchema = \Drupal::database()->schema();
+    $db = \Drupal::database();
+    $dbSchema = $db->schema();
     $configFactory = \Drupal::configFactory();
 
     // Check that the sms tables exist but the others don't.
@@ -66,8 +67,7 @@ class PreviewLinkSessionTokenUpdateTest extends UpdatePathTestBase {
     $this->assertTrue($dbSchema->fieldExists('preview_link', 'entity_type_id'));
     $this->assertTrue($dbSchema->fieldExists('preview_link', 'entity_id'));
 
-    $result = \Drupal::database()->query('SELECT * FROM {preview_link};')->fetchAll();
-    $this->assertEquals('de3a19ee-1edc-4b2e-9af8-f512dddcddcc', $result[0]->token);
+    $this->assertEquals('de3a19ee-1edc-4b2e-9af8-f512dddcddcc', $db->query('SELECT token FROM {preview_link} WHERE id = :id', [':id' => 1])->fetchField());
 
     $this->assertNull($configFactory->get('preview_link.settings')->get('multiple_entities'));
     $definition = \Drupal::entityDefinitionUpdateManager()->getEntityType('preview_link');
